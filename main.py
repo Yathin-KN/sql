d411b6ce2c475baf7010f82c7b580c849e36d6f2
hari vishwanath <mailtohari134@gmail.com>
2024-01-07 14:32:54 +0530

    Add : cors

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from flask import Flask, jsonify
 import mysql.connector
-import time
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app,origins='*')
 
 # Connect to the MySQL database
 mysql_config = {
